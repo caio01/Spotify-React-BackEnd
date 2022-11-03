af90ef6b0c8f940dfb0e45cfbcaad56fde376456
Caio Oliveira <caioinacio00@gmail.com>
2022-11-03 15:38:50 -0300

    add cors lib

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 //Rotas
@@ -10,6 +11,7 @@ const musicsRoute = require('./routes/musicsRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use('/', index)
 app.use('/users', usersRoute)
